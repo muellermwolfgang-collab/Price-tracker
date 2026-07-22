60377328dfb8c0087dd172fca09d4e9ff7b785d4
muellermwolfgang-collab <mueller.m.wolfgang@gmail.com>
2026-07-22 20:47:24 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from . import bike_components, canyon, bike24, fahrrad_de, rosebikes
+
+# All adapters, in the order they'll be tried for each model.
+ADAPTERS = [bike_components, canyon, bike24, fahrrad_de, rosebikes]
